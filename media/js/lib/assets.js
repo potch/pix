e841e4027c10e664685f67ebf244f96a6b8758c9
thepotch <thepotch@gmail.com>
2012-11-18 00:48:54 -0800

    ugh I was using tabs how could I

--- media/js/lib/assets.js
+++ media/js/lib/assets.js
@@ -1,41 +1,41 @@
 var bundles = {
-	root: '',
-	js: {},
-	css: {}
+    root: '',
+    js: {},
+    css: {}
 };
 
 function css(bundle) {
 
-	var assets = bundles.css[bundle];
+    var assets = bundles.css[bundle];
 
-	if (!assets) return;
+    if (!assets) return;
 
-	for (var i = 0; i < assets.length; i++) {
-		var file = bundles.root + 'css/' + assets[i];
-		var tag;
-		if (file.split('.').pop() === 'less') {
-			tag = '<link rel="stylesheet/less" type="text/css" href="' + file + '">';
-		} else {
-			tag = '<link rel="stylesheet" type="text/css" href="' + file + '"/>';
-		}
+    for (var i = 0; i < assets.length; i++) {
+        var file = bundles.root + 'css/' + assets[i];
+        var tag;
+        if (file.split('.').pop() === 'less') {
+            tag = '<link rel="stylesheet/less" type="text/css" href="' + file + '">';
+        } else {
+            tag = '<link rel="stylesheet" type="text/css" href="' + file + '"/>';
+        }
 
-		document.write(tag);
-	}
+        document.write(tag);
+    }
 
 }
 
 function js(bundle) {
-	
-	var assets = bundles.js[bundle]
 
-	if (!assets) return;
+    var assets = bundles.js[bundle]
 
-	for (var i = 0; i < assets.length; i++) {
-		var file = bundles.root + 'js/' + assets[i];
-		var tag = '<script src="' + file + '"></script>';
+    if (!assets) return;
 
-		document.write(tag);
-	}
+    for (var i = 0; i < assets.length; i++) {
+        var file = bundles.root + 'js/' + assets[i];
+        var tag = '<script src="' + file + '"></script>';
+
+        document.write(tag);
+    }
 
 }
 
--- media/js/ui/palette.js
+++ media/js/ui/palette.js
@@ -1,8 +1,11 @@
 define('palette', ['Panel', 'rsvp'], function(Panel, r) {
-	var palette = new Panel({
-		id: 'palette',
-		title: 'Color Palette',
-		closable: true
-	});
-	$('#app').append(palette);
+
+    var palette = new Panel({
+        id: 'palette',
+        title: 'Color Palette',
+        closable: true
+    });
+
+    $('#app').append(palette);
+
 });
